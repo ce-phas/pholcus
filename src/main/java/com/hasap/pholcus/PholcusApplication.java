993b082e8e036ecef3021837f708b7171829a3fa
Pedro Amorim <amorimp@proton.me>
2024-07-04 20:13:39 -0300

    migrate project to Spring Boot

---
+++ src/main/java/com/hasap/pholcus/PholcusApplication.java
@@ -0,0 +1,13 @@
+package com.hasap.pholcus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PholcusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PholcusApplication.class, args);
+	}
+
+}
